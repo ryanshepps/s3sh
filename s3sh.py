8d1705601a38023031ea372568a5dacc27816de3
ryanshepps <sheppardryan@rogers.com>
2023-01-10 08:45:53 -0500

    authenticate user

---
+++ s3sh.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import sys
+import configparser
+import boto3
+
+
+def getAuthenticatedClient():
+    aws_config = configparser.ConfigParser()
+    aws_config.read('S5-S3.conf')
+    return boto3.client(
+        's3',
+        aws_access_key_id=aws_config.get('default', 'aws_access_key_id'),
+        aws_secret_access_key=aws_config.get('default', 'aws_secret_access_key')
+    )
+
+
+def main():
+    print("Welcome to AWS S3 Storage Shell (S5)")
+
+    client = getAuthenticatedClient()
+
+    if client is None:
+        print("You could not be connected to your S3 storage")
+        print("Please review procedures for authenticating your account on AWS S3")
+        sys.exit()
+    else:
+        print("You are now connected to your S3 storage")
+
+
+if "__main__" == __name__:
+    main()
