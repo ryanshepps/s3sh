9b3302d1e8f628ffab876cc4c97c2290a5308a50
ryxn <sheppardryan@rogers.com>
2023-01-11 07:46:43 -0500

    fix error message when user is not authenticated

--- s3sh.py
+++ s3sh.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python3
 
-import sys
 import os
 import configparser
 import boto3
+import botocore
 
 
 def getAuthenticatedClient():
     aws_config = configparser.ConfigParser()
     aws_config.read('S5-S3.conf')
-    return boto3.client(
+    client = boto3.client(
         's3',
-        aws_access_key_id=aws_config.get('default', 'aws_access_key_id'),
-        aws_secret_access_key=aws_config.get('default', 'aws_secret_access_key')
+        aws_access_key_id=aws_config.get("default", "aws_access_key_id"),
+        aws_secret_access_key=aws_config.get("default", "aws_secret_access_key")
     )
+
+    try:
+        client.list_buckets()
+        print("You are now connected to your S3 storage")
+        return client
+    except botocore.exceptions.ClientError:
+        print("You could not be connected to your S3 storage")
+        print("Please review procedures for authenticating your account on AWS S3")
+        return None
 
 
 def main():
@@ -21,14 +30,7 @@ def main():
 
     client = getAuthenticatedClient()
 
-    if client is None:
-        print("You could not be connected to your S3 storage")
-        print("Please review procedures for authenticating your account on AWS S3")
-        sys.exit()
-    else:
-        print("You are now connected to your S3 storage")
-
-    while True:
+    while client is not None:
         command = input("S5> ")
         if command == "exit":
             break
